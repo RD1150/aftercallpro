6fdba04345816cb275a637e645d766f93f56e1ea
RD1150 <reenadutta99@yahoo.com>
2025-11-30 23:15:19 -0800

    Update vite.config.js

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,14 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwind from "@tailwindcss/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+  plugins: [react(), tailwind()],
+});
