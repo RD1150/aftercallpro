210ee30a4ef5a1d309424a0b41d6f1ab4fb3e6ea
RD1150 <reenadutta99@yahoo.com>
2025-12-10 22:32:35 -0800

    Update tailwind.config.js

--- src/frontend/tailwind.config.js
+++ src/frontend/tailwind.config.js
@@ -1,10 +1,20 @@
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/pages/**/*.{js,jsx}",
+    "./src/components/**/*.{js,jsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: "#0b1524",
+        cyan: "#22d3ee",
+        graylight: "#f8fafc",
+        graydark: "#1e293b",
+      },
+    },
   },
   plugins: [],
 };
