61a2b8889134d3ef1ee62757b60a06b2cdb3d7dc
RD1150 <reenadutta99@yahoo.com>
2026-01-02 13:27:04 -0800

    Update SMSConsentPage.jsx

--- src/frontend/src/components/SMSConsentPage.jsx
+++ src/frontend/src/components/SMSConsentPage.jsx
@@ -1,25 +1,16 @@
 import React from "react";
-import LegalLayout from "./LegalLayout";
+import LegalLayout from "../components/LegalLayout";
 
 export default function SMSConsentPage() {
   return (
-    <LegalLayout title="SMS Consent" lastUpdated="December 2025">
-
-      <section>
-        <p>
-          By providing your phone number, you consent to receive transactional
-          and service-related SMS messages from AfterCallPro.
-        </p>
-      </section>
-
-      <section>
-        <ul className="list-disc pl-6">
-          <li>Message frequency varies</li>
-          <li>Message & data rates may apply</li>
-          <li>Reply STOP to opt out</li>
-        </ul>
-      </section>
-
+    <LegalLayout
+      title="SMS Consent"
+      lastUpdated="December 2025"
+    >
+      <p>
+        By providing your phone number, you consent to receive transactional SMS
+        messages from AfterCallPro. Reply STOP to opt out.
+      </p>
     </LegalLayout>
   );
 }
