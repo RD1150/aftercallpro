4148e588538f255eb7bde27965424de3e48a4955
RD1150 <reenadutta99@yahoo.com>
2026-01-02 18:12:36 -0800

    Update vite.config.js

--- src/frontend/vite.config.js
+++ src/frontend/vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "../backend/templates",
+    outDir: path.resolve(__dirname, "../backend/templates"),
     emptyOutDir: true,
-    assetsDir: "assets",
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name]-[hash][extname]",
+        entryFileNames: "assets/[name]-[hash].js",
+      },
+    },
   },
 });
