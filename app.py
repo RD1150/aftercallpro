fcb1234a57e0f8efeae5a6d3ec7b0e3ee53f4e45
RD1150 <reenadutta99@yahoo.com>
2026-01-04 14:33:21 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html")
 
 @app.route("/privacy")
@@ -13,6 +13,3 @@ def privacy():
 @app.route("/terms")
 def terms():
     return render_template("terms.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
