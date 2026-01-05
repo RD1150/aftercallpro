33664371548a368c42be1990db0f53c60423d204
RD1150 <reenadutta99@yahoo.com>
2026-01-05 13:28:14 -0800

    Update app.py

--- app.py
+++ app.py
@@ -20,3 +20,5 @@ def sms_consent():
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5050, debug=True)
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=5050, debug=True)
