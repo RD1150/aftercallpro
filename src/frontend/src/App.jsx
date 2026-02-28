379672756edc367cc14823008eadb64f47772ee4
RD1150 <reenadutta99@yahoo.com>
2026-02-27 20:09:26 -0800

    Update App.jsx

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
@@ -7,19 +7,15 @@ export default function App() {
   const token = localStorage.getItem("token");
 
   return (
-    <Router>
-      <Routes>
-        {/* Root = Signup */}
-        <Route path="/" element={<Signup />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+    <Routes>
+      <Route path="/" element={<Signup />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
 
-        {/* Protected route */}
-        <Route
-          path="/dashboard"
-          element={token ? <Dashboard /> : <Navigate to="/login" />}
-        />
-      </Routes>
-    </Router>
+      <Route
+        path="/dashboard"
+        element={token ? <Dashboard /> : <Navigate to="/login" />}
+      />
+    </Routes>
   );
 }
