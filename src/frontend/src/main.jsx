145a8e030aeec5d4381338c1d2a80008f92137f8
RD1150 <reenadutta99@yahoo.com>
2026-03-01 10:45:14 -0800

    Update main.jsx

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { AuthProvider } from "./AuthProvider.jsx";
+import { AuthProvider } from "./AuthProvider";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
