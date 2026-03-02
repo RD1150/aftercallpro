71e8468c274424de88c1b8a3c034362ecebf8587
RD1150 <reenadutta99@yahoo.com>
2026-03-02 00:45:29 -0800

    Update App.jsx

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,11 +1,13 @@
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<div>HOME WORKS</div>} />
+        <Route path="/" element={<Dashboard />} />
       </Routes>
     </Router>
   );
