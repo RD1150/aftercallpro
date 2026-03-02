150def1ec35acc096e4b428c88fd2da990af4e68
RD1150 <reenadutta99@yahoo.com>
2026-03-02 00:29:51 -0800

    Update App.jsx

--- src/frontend/src/App.jsx
+++ src/frontend/src/App.jsx
@@ -1,19 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <div style={{ padding: "40px", fontSize: "24px" }}>
+      AFTERCALLPRO TEST SCREEN
+    </div>
   );
 }
 
