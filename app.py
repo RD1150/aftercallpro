6afd114d0a2ad7ed0957d6452e49bfb5f81b1556
RD1150 <reenadutta99@yahoo.com>
2026-03-02 23:26:54 -0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,16 @@ app.secret_key = os.environ.get("SECRET_KEY", "dev-secret-change-this")
 # -------------------------
 # DATABASE CONFIG
 # -------------------------
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+database_url = os.environ.get("DATABASE_URL")
+
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql+psycopg://",
+        1
+    )
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 # -------------------------
